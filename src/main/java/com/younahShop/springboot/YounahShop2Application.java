4939c6c3010e0acf41b9c5e1ce214bbe4d9ef290
younah shin <roaving@gmail.com>
2019-12-21 10:50:42 +0900

    first

---
+++ src/main/java/com/younahShop/springboot/YounahShop2Application.java
@@ -0,0 +1,13 @@
+package com.younahShop.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YounahShop2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YounahShop2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/younahShop/springboot/YounahShop2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.younahShop.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YounahShop2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
